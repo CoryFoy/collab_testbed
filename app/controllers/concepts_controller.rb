fe4bffd578e8a3836af37fff5d2966bdea1f2db3
Joey Shipley <joey.shipley@gmail.com>
2012-08-22 16:41:47 -0400

    Adds search by publications to Concept:One

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -6,26 +6,16 @@ class ConceptsController < ApplicationController
     query = params[:query]
     ra_id = params[:research_area_id]
 
+    @research_areas_options = ResearchArea.all
     build_form_elements_for_one
 
-    unless ra_id.nil?
-      @research_area = ResearchArea.find_by_id(ra_id)
-
-      @publications = @research_area.publications
-      @researchers = @research_area.researchers.group(:id)
-      @research_areas = @research_area.research_areas.group(:id)
-      @grants = @research_area.grants
-      @patents = @research_area.patents
-      @departments = @research_area.departments.group(:id)
-      @venues = @research_area.venues.group(:id)
-    end
+    build_research_area_by ra_id unless ra_id.blank?
+    build_publication_by query unless query.blank?
   end
 
   private
 
   def build_form_elements_for_one
-    @research_areas_options = ResearchArea.all
-
     @departments = []
     @researchers = []
     @venues = []
@@ -35,4 +25,29 @@ class ConceptsController < ApplicationController
     @patents = []
   end
 
+  def build_research_area_by(ra_id)
+    @research_area = ResearchArea.find_by_id(ra_id)
+    @selected_name = @research_area.name
+
+    @publications = @research_area.publications
+    @researchers = @research_area.researchers.group(:id)
+    @research_areas = @research_area.research_areas.group(:id)
+    @grants = @research_area.grants
+    @patents = @research_area.patents
+    @departments = @research_area.departments.group(:id)
+    @venues = @research_area.venues.group(:id)
+  end
+
+  def build_publication_by(query)
+    @publication = Publication.where("name LIKE ?", "%#{query}%").first
+    @selected_name = @publication.name
+
+    @research_areas = @publication.research_areas
+    @publications = @publication.publications.group(:id)
+    @researchers = @publication.researchers
+    @departments = @publication.departments.group(:id)
+    @venues = [] << @publication.venue
+    @patents = @publication.patents
+    @grants = @publication.grants
+  end
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,5 +3,10 @@ class Publication < ActiveRecord::Base
   has_and_belongs_to_many :research_areas
   belongs_to :venue
 
+  has_many :publications, :through => :research_areas
+  has_many :departments, :through => :researchers
+  has_many :patents, :through => :research_areas
+  has_many :grants, :through => :research_areas
+
   attr_accessible :name, :venue_id
 end
